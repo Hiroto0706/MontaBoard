6a8921f0c51da8df894339718726088ac19bba78
Hiroto0706 <mirunina2366@icloud.com>
2022-08-16 10:31:22 +0900

    cssをすこし修正しました。ログインボタンと登録ボタンを作成しました。

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -7,3 +7,7 @@ import (
 func top(w http.ResponseWriter, r *http.Request) {
 	generateHTML(w, "スレッド一覧", "layout", "top")
 }
+
+func thread(w http.ResponseWriter, r *http.Request) {
+	generateHTML(w, nil, "layout", "thread")
+}
